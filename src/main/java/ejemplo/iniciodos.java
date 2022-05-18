4646fc4c90b1456aa681bb7d1cc017bd432875ba
diego <diego.cortinagarcia@gmail.com>
2022-05-18 12:39:19 +0200

    added name to iniciodos

--- src/main/java/ejemplo/iniciodos.java
+++ src/main/java/ejemplo/iniciodos.java
@@ -5,6 +5,8 @@ public class iniciodos {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hola mundoo");
+		String nombre = "Diego";
+		System.out.println(nombre);
 	}
 
 }
